7c35d281faad96fb35bf88ae98643f6d31db7e2e
samiullah-sahir <ss.samiullah2020@gmail.com>
2022-10-10 19:13:06 +0430

    Navbar design completion.

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-var name = "Hello world";
+const nav = document.querySelector('.navbar');
+
+const linksFadOut = function () {
+  const handleHover = function (e, opacity) {
+    if (e.target.classList.contains('nav-link')) {
+      const link = e.target;
+
+      const siblings = link.closest('.navbar').querySelectorAll('.nav-link');
+
+      const logo = link.closest('.navbar').querySelector('.navbar-brand');
+
+      siblings.forEach((el) => {
+        if (el !== link) el.style.opacity = opacity;
+      });
+      logo.style.opacity = opacity;
+    }
+  };
+
+  nav.addEventListener('mouseover', (e) => {
+    handleHover(e, 0.5);
+  });
+
+  nav.addEventListener('mouseout', (e) => {
+    handleHover(e, 1);
+  });
+};
+linksFadOut();
